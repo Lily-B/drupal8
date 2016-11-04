c2cc552948ebb539dc61480905e609acc69ba69e
LilyB <LilyB.php@gmail.com>
2016-11-04 17:59:17 +0200

    november theme  adds custom library in the theme with simple  log.js file

---
+++ themes/custom/november/js/log.js
@@ -0,0 +1,8 @@
+/**
+ * Created by root on 04.11.16.
+ */
+(function ($, Drupal, drupalSettings) {
+    $(document).ready(function () {
+       console.log("Custom library works!");
+    });
+})(jQuery, Drupal, drupalSettings);
